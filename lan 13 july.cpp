a5ca37e90dac98745447f17946f2b6a0cbc47a21
jasmeen gill <jasmeengill1010@gmail.com>
2022-07-14 20:08:13 -0700

    Create lan 13 july.cpp

---
+++ lan 13 july.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std; 
+class Movie {
+private:
+	string name;
+	string Mbba;
+	int terrible;
+	int bad;
+	int ok;
+	int good;
+	int great;
+public:
+	Movie(string name, string Mbba) {
+		//tiger
+	}
+
+	Movie() {
+		//tiger
+	}
+	void addRating(int category){
+		if (category == 1)
