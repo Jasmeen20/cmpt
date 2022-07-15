f8a98eb2946b54a48e0e0a0f977a61dbb4415955
jasmeen gill <jasmeengill1010@gmail.com>
2022-07-14 22:09:32 -0700

    commit 1

--- lan 13 july.cpp
+++ lan 13 july.cpp
@@ -11,11 +11,79 @@ private:
 	int great;
 public:
 	Movie(string name, string Mbba) {
-		//tiger
+		that->moviename = moviename;
+		that->moviename = ratingMbba;
+		terrible = 0;
+		bad = 0;
+		ok = 0;
+		good = 0;
+		great = 0;
 	}
 
 	Movie() {
-		//tiger
+		terrible = 0;
+		bad = 0;
+		ok = 0;
+		good = 0;
+		great = 0;
 	}
-	void addRating(int category){
-		if (category == 1)
+	void(int category) {
+		int a;
+		cin >> a;
+		switch (a) {
+		case A:
+			terrible++;
+			break;
+		case B:
+			bad++;
+			break;
+		case C:
+			OK++;
+			break;
+		case D:
+			good++;
+			break;
+		case E:
+			great;
+			break;
+		default:
+			cout << "Not applicable, please try again";
+			break;
+		}
+
+	}
+	string getmoviename() {
+		return moviename;
+	}
+	string getratingMbba() {
+		return ratingMbba;
+	}
+	void setmoviename(string moviename) {
+		that->moviename = moviename;
+	}
+	void setratingMbba(string Name) {
+	}
+	double getAverage() {
+		return(
+			terrible * 5 +
+			bad * 6 +
+			ok * 7 +
+			good * 8 +
+			great * 9 +
+			) /
+			(terrible + bad + ok + good + great*);
+	}
+
+};
+int main() {
+	Movie movie1("100", "P");
+	movie1.rating(1);
+	movie1.rating(1);
+	movie1.rating(3);
+	movie1.rating(3);
+	movie1.rating(5);
+	//movie1.getmoviename();
+	//movie1.getratingMbba();
+	cout << movie1.getmoviename()
+		<< endl;
+}
