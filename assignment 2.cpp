a8ac3381e9591fd87f3809a49384d8199ac4f256
jasmeen gill <jasmeengill1010@gmail.com>
2022-07-20 12:39:08 -0700

    assignmet 1

---
+++ assignment 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Rational number
+{
+	int num, den;
+public:
+	void read()
+	{
+		cout << "Enter Denominator for Rational Number"
+			<< endl;
+		cin >> den;
+		cout << "Enter Numerator for Rational Number"
+			<< endl;
+		cin >> num;
+	}
+	
+		
